14757cd751b3a7d04b0963543a8a730e32d8efbd
Glen Correia <glen_correia@outlook.com>
2025-11-08 07:33:13 +0530

    Inverted Right Half Pyramid

---
+++ inverted-right-half-pyramid.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+  int rows, i, j;
+
+  printf("Enter the number of rows: ");
+  scanf("%d", &rows);
+
+  for (i = rows; i > 0; i--) {
+    for (j = 1; j <= i; j++) {
+      printf("* ");
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
+
+/* OUTPUT: Inverted Right Half Pyramid
+
+  Enter the number of rows: 5
+  * * * * *
+  * * * * 
+  * * *
+  * *
+  * 
+*/
