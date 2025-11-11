21c811ca9bfc05e4cc2df7a7da5a8b5e81665eef
Glen Correia <glen_correia@outlook.com>
2025-11-11 15:36:40 +0530

    Update

---
+++ 7_rhombus-pattern.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+  int rows, i, j;
+
+  printf("Enter the number of rows: ");
+  scanf("%d", &rows);
+
+  for (i = 1; i <= rows; i++) {
+    for (j = rows - i; j < rows; j++) {
+      printf(" ");
+    }
+
+    for (j = rows; j >= 1; j--) { // by replacing rows here with an absolute number, we can make the number of characters appear in constant on each line
+      printf(" +");
+    }
+
+    printf("\n");
+  }
+
+  return 0;
+}
+
+/* OUTPUT: Rhombus Pattern
+
+  Enter the number of rows: 5
+   + + + + +
+    + + + + +
+     + + + + +
+      + + + + + 
+       + + + + +
+*/
